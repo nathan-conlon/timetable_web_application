25d95112d9f5c48f87946e2ca4a3b1735228c315
Nathan Conlon <3057234@ads.qub.ac.uk>
2024-07-10 15:41:15 +0100

    Final updates before server testing

--- getExcelFile.js
+++ getExcelFile.js
@@ -4,39 +4,33 @@ async function main() {
   const browser = await puppeteer.launch({
     headless: true,
     executablePath:
-      "C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe",
+      "C:/Program files (x86)/Microsoft/Edge/Application/msedge.exe",
     userDataDir: "./data",
   });
 
-  const page = await browser.newPage();
+  const page = await browser.newPage(); // Open new page
 
   const client = await page.target().createCDPSession();
 
+  // Delete the existing file before triggering the download
+  // const downloadFilePath = path.join(__dirname, "filename.ext"); // Adjust the filename and extension as needed
+  // try {
+  //   await fs.unlink(downloadFilePath); // Attempt to delete the file
+  //   console.log(`Deleted existing file: ${downloadFilePath}`);
+  // } catch (error) {
+  //   // If the file does not exist or there's an error deleting it, ignore the error
+  //   console.log(`No existing file to delete: ${downloadFilePath}`);
+  // }
+
+  // Set download behavior to allow and specify download path
   await client.send("Page.setDownloadBehavior", {
     behavior: "allow",
     downloadPath: __dirname,
   });
 
   await page.goto(
-    "https://qubstudentcloud-my.sharepoint.com/personal/1324764_ads_qub_ac_uk/_layouts/15/download.aspx?UniqueId=%7B0a91ec72-9ee2-47df-9314-e6a2a70df4a8%7D"
+    "https://qubstudentcloud-my.sharepoint.com//personal//1324764_ads_qub_ac_uk//_layouts//15//download.aspx?UniqueId=%7B0a91ec72%2D9ee2%2D47df%2D9314%2De6a2a70df4a8%7D"
   );
-
-  // Add any additional actions here, such as taking a screenshot or scraping data
-
-  // Close the browser when done with this iteration
-  await browser.close();
-
-  console.log("Successfully executed main function.");
-
-  // Return a promise resolved after a short delay (simulating a 10-second interval)
-  return new Promise((resolve) => setTimeout(resolve, 10000));
 }
 
-// Function to run main function every 10 seconds
-async function runEvery10Seconds() {
-  while (true) {
-    await main().catch(console.error);
-  }
-}
-
-runEvery10Seconds();
+main().catch(console.error);
